098b3014601e44230cb426397af7b92aaab016ab
handoing <chenhandoing@gmail.com>
2020-08-06 18:52:20 +0800

    init

---
+++ tests/lib/rules/no-global.js
@@ -0,0 +1,37 @@
+/**
+ * @fileoverview demo
+ * @author demo
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+var rule = require("../../../lib/rules/no-global"),
+
+    RuleTester = require("eslint").RuleTester;
+
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+var ruleTester = new RuleTester();
+ruleTester.run("no-global", rule, {
+
+    valid: [
+
+        // give me some code that won't trigger a warning
+    ],
+
+    invalid: [
+        {
+            code: "",
+            errors: [{
+                message: "Fill me in.",
+                type: "Me too"
+            }]
+        }
+    ]
+});
